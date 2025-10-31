1726851bb6836c03e29425fcf32ea7261ebe94b7
santiago <santiago@santiago-T140J-Sargas-2025>
2025-10-31 09:54:27 -0300

    test

--- final/src/Exe/Exe.java
+++ final/src/Exe/Exe.java
@@ -6,6 +6,6 @@ import UI.UI;
 
 public class Exe {
 	public static void main(String[] args) throws SQLException {
-		new UI(new GestorPersona(new PersonaDB())).setVisible(true); // algo
+		new UI(new GestorPersona(new PersonaDB())).setVisible(true); // algoa
 	}
 }
