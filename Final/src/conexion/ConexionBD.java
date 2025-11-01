a4369cdda15d04054cfa3d5a3a71d6b0fbf363ed
santiago <santiago@archlinux>
2025-11-01 15:40:10 -0300

    Update en el funcionamiento general de ConexionBD.

--- Final/src/conexion/ConexionBD.java
+++ Final/src/conexion/ConexionBD.java
@@ -1,160 +1,101 @@
 package conexion;
 
 import modelo.Prenda;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 
 public class ConexionBD {
-	private ArrayList<Prenda> lista;
-	private Connection con;
+    private Connection getConexion() throws SQLException {
+        return DriverManager.getConnection("jdbc:mariadb://localhost:3306/prendasDB", "root", "test");
+    }
 
-	public ConexionBD() throws SQLException { //FIXME
-		try {
-			Class.forName("org.mariadb.jdbc.Driver");
-			con = DriverManager.getConnection("jdbc:mariadb://localhost:3306/prendasDB", "root", "test");
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-			System.exit(1);
-		}
-	}
-	
-	private Prenda crearPrenda(ResultSet rs) throws SQLException {
-//								ID			nombre			apellido		edad			estudiante		sexo			pa'is			pa'isIndex
-		return new Prenda(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getInt(4), rs.getBoolean(5), rs.getString(6), rs.getString(7), rs.getInt(8)); //FIXME
-	}
+    private Prenda crearPrenda(ResultSet rs) throws SQLException {
+        // FIXME
+    	return new Prenda(rs.getInt("id_prenda"), rs.getString("nombre"), rs.getString("apellido"), rs.getInt("edad"), rs.getBoolean("estudiante"), rs.getString("sexo"), rs.getString("pais"), rs.getInt("paisIndex"));
+    }
 
-	public ArrayList<Prenda> obtenerTodo() throws SQLException {
-		lista = new ArrayList<Prenda>();
-		try {
-			Statement st = con.createStatement();
-			ResultSet rs = st.executeQuery("SELECT * FROM prenda");
+    public ArrayList<Prenda> obtenerTodo() throws SQLException {
+        ArrayList<Prenda> prendas = new ArrayList<>();
 
-			while (rs.next()) { lista.add(crearPrenda(rs)); }
+        try (Connection con = getConexion();
+             Statement st = con.createStatement();
+             ResultSet rs = st.executeQuery("SELECT * FROM prenda")) {
 
-			rs.close();
-			st.close();
-			con.close();
-		} catch (SQLException e) {
-			System.out.println(e.getMessage());
-		}
+            while (rs.next()) { prendas.add(crearPrenda(rs)); }
+        } catch(SQLException e) {
+        	System.out.println(e.getMessage());
+        }
 
-		return lista;
-	}
+        return prendas;
+    }
 
-	public Prenda obtenerPrenda(int id) throws SQLException {
-		Prenda p = null;
+    public Prenda obtenerPrenda(int id) throws SQLException {
+        Prenda prenda = null;
 
-		try {
-			if (lista.get(id - 1) != null) { return lista.get(id - 1); }
+        try (Connection con = getConexion();
+             PreparedStatement pst = con.prepareStatement("SELECT * FROM prenda WHERE id_prenda=?")) {
 
-			PreparedStatement pst = con.prepareStatement("SELECT * FROM prenda WHERE id_prenda=?");
+            pst.setInt(1, id);
+            
+            try (ResultSet rs = pst.executeQuery()) {
+                if (rs.next()) { prenda = crearPrenda(rs); }
+            }
+        } catch(SQLException e) {
+        	System.out.println(e.getMessage());
+        }
 
-			pst.setInt(1, id);
+        return prenda;
+    }
 
-			ResultSet rs = pst.executeQuery();
-			p = crearPrenda(rs);
+    public boolean insertarPrenda(Prenda p) throws SQLException {
+        try (Connection con = getConexion();
+             PreparedStatement pst = con.prepareStatement("INSERT INTO prenda(nombre, apellido, edad, estudiante, sexo, pais, paisIndex) VALUES(?, ?, ?, ?, ?, ?, ?)")) {
 
-			rs.close();
-			pst.close();
-//			con.close();
-		} catch (SQLException e) {
-			System.out.println(e.getMessage());
-		}
+            pst.setString(1, p.getNombre());
+            pst.setString(2, p.getApellido());
+            pst.setInt(3, p.getEdad());
+            pst.setBoolean(4, p.getEstudiante());
+            pst.setString(5, p.getSexo());
+            pst.setString(6, p.getPais());
+            pst.setInt(7, p.getPaisIndex());
 
-		return p;
-	}
+            return pst.executeUpdate() > 0;
+        } catch(SQLException e) {
+        	System.out.println(e.getMessage());
+        	return false;
+        }
+    }
 
-	public void insertarPrenda(Prenda p) throws SQLException {
-		try {
-			PreparedStatement st = con.prepareStatement("INSERT INTO prenda(nombre, apellido, edad, estudiante, sexo, pais, paisIndex) VALUES(?, ?, ?, ?, ?, ?, ?)");
+    public boolean modificarPrenda(Prenda p) throws SQLException {
+        try (Connection con = getConexion();
+             PreparedStatement pst = con.prepareStatement("UPDATE prenda SET nombre=?, apellido=?, edad=?, estudiante=?, sexo=?, pais=?, paisIndex=? WHERE id_prenda=?")) {
 
-			st.setString(1, p.getNombre());
-			st.setString(2, p.getApellido());
-			st.setInt(3, p.getEdad());
-			st.setBoolean(4, p.getEstudiante());
-			st.setString(5, p.getSexo());
-			st.setString(6, p.getPais());
-			st.setInt(7, p.getPaisIndex());
+            pst.setString(1, p.getNombre());
+            pst.setString(2, p.getApellido());
+            pst.setInt(3, p.getEdad());
+            pst.setBoolean(4, p.getEstudiante());
+            pst.setString(5, p.getSexo());
+            pst.setString(6, p.getPais());
+            pst.setInt(7, p.getPaisIndex());
+            pst.setInt(8, p.getId());
 
-			if (st.executeUpdate() != 0) {
-				if (!lista.contains(p)) {
-					if (lista.size() > 1) {
-						lista.set(p.getIndex(), p);
-					} else {
-						lista.add(p);
-					}
-				}
-//				TODO
-			}
+            return pst.executeUpdate() > 0;
+        }
+    }
 
-			st.close();
-//			con.close();
-		} catch (SQLException e) {
-			System.out.println(e.getMessage());
-		}
-	}
+    public boolean eliminarPrenda(int id) throws SQLException {
+        try (Connection con = getConexion();
+             PreparedStatement pst = con.prepareStatement("DELETE FROM prenda WHERE id_prenda=?")) {
 
-	public void modificarPrenda(Prenda p) throws SQLException {
-		try {
-			PreparedStatement pst = con.prepareStatement("UPDATE prenda SET nombre=?, apellido=?, edad=?, estudiante=?, sexo=?, pais=?, paisIndex=? WHERE id_prenda=?");
+            pst.setInt(1, id);
+            return pst.executeUpdate() > 0;
+        }
+    }
 
-			pst.setString(1, p.getNombre());
-			pst.setString(2, p.getApellido());
-			pst.setInt(3, p.getEdad());
-			pst.setBoolean(4, p.getEstudiante());
-			pst.setString(5, p.getSexo());
-			pst.setString(6, p.getPais());
-			pst.setInt(7, p.getPaisIndex());
-			pst.setInt(8, p.getId());
-
-			if (pst.executeUpdate() != 0) {
-				lista.set(p.getIndex(), p);
-//				TODO
-			}
-
-			pst.close();
-//			con.close();
-		} catch (SQLException e) {
-			System.out.println(e.getMessage());
-		}
-	}
-
-	public void eliminarPrenda(Prenda p) throws SQLException {
-		try {
-			PreparedStatement pst = con.prepareStatement("DELETE FROM prenda WHERE id_prenda=?");
-
-			pst.setInt(1, p.getId());
-
-			if (pst.executeUpdate() != 0) {
-				lista.remove(p);
-//				TODO
-			}
-
-			pst.close();
-//			con.close();
-		} catch (SQLException e) {
-			System.out.println(e.getMessage());
-		}
-	}
-
-	public void eliminarTodo() throws SQLException {
-		try {
-			PreparedStatement pst = con.prepareStatement("TRUNCATE TABLE prenda");
-
-			if (pst.executeUpdate() != 0) {
-//				TODO
-			}
-
-			pst.close();
-//			con.close();
-		} catch (SQLException e) {
-			System.out.println(e.getMessage());
-		}
-	}
+    public void eliminarTodo() throws SQLException {
+        try (Connection con = getConexion();
+             PreparedStatement pst = con.prepareStatement("TRUNCATE TABLE prenda")) {
+            pst.executeUpdate();
+        }
+    }
 }
