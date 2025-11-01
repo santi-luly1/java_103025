f47ad770c51bfac02424eaf2ce53b62f59d7e1b3
santiago <santiago@archlinux>
2025-11-01 11:27:31 -0300

    restart

--- final/src/main/PrendaInvalidaException.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-public class PersonaInvalidaException extends Exception {
-	private static final long serialVersionUID = 1L;
-
-	public PersonaInvalidaException(String mensaje) {
-		super(mensaje);
-	}
-}
