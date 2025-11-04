c3e9b5bcdb267c3c58579d078b358b9aac7f5918
santiluly1 <santiagotoranza8@gmail.com>
2025-11-04 14:51:11 -0300

    implementaci'on de los atributos en el DAO

--- Final/src/dao/PrendaDAO.java
+++ Final/src/dao/PrendaDAO.java
@@ -4,6 +4,13 @@ import java.util.List;
 import modelo.Prenda;
 
 public interface PrendaDAO {
+	int id = 0;
+	String descripcion = "";
+	String talla = "";
+	String color = "";
+	double precio = 0;
+	int stock = 0;
+	
 	void agregar(Prenda prenda);
 	Prenda obtenerPorId(int id);
 	List<Prenda> listarTodas();
